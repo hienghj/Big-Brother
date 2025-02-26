b8b2bd4e2d8b18b49e1281214f569d0aeea6fbe8
Dio10072003 <dunghuynh1231231@gmail.com>
2025-02-26 14:56:08 +0700

    add

--- Big-Brother/obj/Debug/net8.0/Big-Brother.AssemblyInfo.cs
+++ Big-Brother/obj/Debug/net8.0/Big-Brother.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Big-Brother")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a3c739bb392c45d8ab59e5641980714b2f7af1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc424e8298a4ebf4b6d02a4286d6d6615573638")]
 [assembly: System.Reflection.AssemblyProductAttribute("Big-Brother")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Big-Brother")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
